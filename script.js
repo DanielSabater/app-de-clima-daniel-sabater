a13bf6bcc3c6111093d801c3c1fe6cb821649467
DanielSabater <111099646+DanielSabater@users.noreply.github.com>
2025-09-23 01:52:06 -0300

    casi finalizado

--- script.js
+++ script.js
@@ -17,25 +17,35 @@ function fetchDatosClima(ciudad) {
 }
 
 function mostrarDatosClima (data) {
-    console.log(data)
     const divDatosClima = document.getElementById('datosClima')
     divDatosClima.innerHTML=''
 
     const ciudadNombre = data.name
+    const paisNombre = data.sys.country
+    const humedad = data.main.humidity
     const temperatura = data.main.temp
+    const icono = data.weather[0].icon
     const descripcion = data.weather[0].description
 
     const ciudadTitulo = document.createElement ('h2')
-    ciudadTitulo.textContent = ciudadNombre
+    ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`
 
     const temperaturaInfo = document.createElement ('p')
-    temperaturaInfo.textContent = `La temperatura es ${Math.floor(temperatura-difKelvin)}°C`
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)}°C`
+
+    const humedadInfo = document.createElement ('p')
+    humedadInfo.textContent = `La Humedad es de: ${humedad}%`
+
+    const iconoInfo = document.createElement ('img')
+    iconoInfo.src = `https://openweathermap.org/img/wn/${icono}@2x.png`
 
     const descripcionInfo = document.createElement ('p')
     descripcionInfo.textContent = `Descripcion Meteorologiaca: ${descripcion}`
 
     divDatosClima.appendChild(ciudadTitulo)
     divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(humedadInfo)
+    divDatosClima.appendChild(iconoInfo)
     divDatosClima.appendChild(descripcionInfo)
 }
 
